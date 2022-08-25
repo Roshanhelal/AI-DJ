e8f6d9b27a96a521f552b78d83c7834c953e3731
Roshanhelal <98272607+Roshanhelal@users.noreply.github.com>
2022-08-25 20:46:38 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-var song="";
-
+var song=""; 
+var left_wrist_X=0;
+var left_wrist_Y=0;
+var right_wrist_X=0;
+var right_wrist_Y=0;
 function preload(){
     song=loadSound("music.mp3")
 }
@@ -8,8 +11,14 @@ function setup(){
     canvas=createCanvas(600,500);
     canvas.center();        
 
+  
+    
+
     video=createCapture(VIDEO);
     video.hide();
+
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotpose);
 }
 
 function draw(){
@@ -18,5 +27,25 @@ function draw(){
 }
 
 function play(){
-    song.play()
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
+
+function modelLoaded(){
+    console.log("Model is loaded");
+
+}
+function gotpose(results){
+if(results.length>0)
+{
+    console.log(results);
+    left_wrist_X=results[0].pose.leftWrist.x;
+    left_wrist_Y=results[0].pose.leftWrist.y;
+    console.log("left Wrist x="+left_wrist_X+"left wrist y="+left_wrist_Y);
+    right_wrist_X=results[0].pose.rightWrist.x;
+    right_wrist_Y=results[0].pose.rightWrist.y;
+    console.log("right Wrist x="+right_wrist_X+"right wrist y="+right_wrist_Y);
+}
+
 }
